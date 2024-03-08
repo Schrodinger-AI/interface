be5d16d67051673351f88295fd71f8fd1fe8d1b5
Desmondwi <desmond@schrodingernft.ai>
2024-03-08 15:04:50 +0800

    fix: 🐛 do not show domain

--- src/components/HostTag/index.tsx
+++ src/components/HostTag/index.tsx
@@ -16,27 +16,31 @@ export function NavHostTag() {
 
   return (
     <>
-      <div className={styles.navHostTag} onClick={() => setOpen(true)}>
-        {responsive.md ? (
-          <Tooltip color="black" title={hostName} overlayInnerStyle={{ color: 'white' }}>
-            {hostStr}
-          </Tooltip>
-        ) : (
-          hostStr
-        )}
-      </div>
-      {responsive.md ? null : (
-        <Modal
-          centered
-          open={open}
-          footer={
-            <Button type="primary" className={styles.modalBtn} onClick={() => setOpen(false)}>
-              OK
-            </Button>
-          }
-          onCancel={() => setOpen(false)}>
-          {hostName}
-        </Modal>
+      {hostName && (
+        <>
+          <div className={styles.navHostTag} onClick={() => setOpen(true)}>
+            {responsive.md ? (
+              <Tooltip color="black" title={hostName} overlayInnerStyle={{ color: 'white' }}>
+                {hostStr}
+              </Tooltip>
+            ) : (
+              hostStr
+            )}
+          </div>
+          {responsive.md ? null : (
+            <Modal
+              centered
+              open={open}
+              footer={
+                <Button type="primary" className={styles.modalBtn} onClick={() => setOpen(false)}>
+                  OK
+                </Button>
+              }
+              onCancel={() => setOpen(false)}>
+              {hostName}
+            </Modal>
+          )}
+        </>
       )}
     </>
   );
@@ -52,28 +56,32 @@ export function HomeHostTag() {
 
   return (
     <>
-      <div className={styles.homeHostTag} onClick={() => setOpen(true)}>
-        {responsive.md ? (
-          <Tooltip title={hostName} color="black" overlayInnerStyle={{ color: 'white' }}>
-            {hostStr}
-          </Tooltip>
-        ) : (
-          hostStr
-        )}
-      </div>
-      {responsive.md ? null : (
-        <Modal
-          title="Invited by"
-          centered
-          open={open}
-          footer={
-            <Button type="primary" className={styles.modalBtn} onClick={() => setOpen(false)}>
-              OK
-            </Button>
-          }
-          onCancel={() => setOpen(false)}>
-          {hostName}
-        </Modal>
+      {hostName && (
+        <>
+          <div className={styles.homeHostTag} onClick={() => setOpen(true)}>
+            {responsive.md ? (
+              <Tooltip title={hostName} color="black" overlayInnerStyle={{ color: 'white' }}>
+                {hostStr}
+              </Tooltip>
+            ) : (
+              hostStr
+            )}
+          </div>
+          {responsive.md ? null : (
+            <Modal
+              title="Invited by"
+              centered
+              open={open}
+              footer={
+                <Button type="primary" className={styles.modalBtn} onClick={() => setOpen(false)}>
+                  OK
+                </Button>
+              }
+              onCancel={() => setOpen(false)}>
+              {hostName}
+            </Modal>
+          )}
+        </>
       )}
     </>
   );
