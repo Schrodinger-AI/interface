caf132b41c5b607948fa3d1881e71fb436870273
Desmondwi <desmond@schrodingernft.ai>
2024-03-11 18:26:17 +0800

    style: 💄 adjust image preview in mobile

--- src/components/AIImageSelect/index.tsx
+++ src/components/AIImageSelect/index.tsx
@@ -1,7 +1,8 @@
-import { Image } from 'antd';
+import { Image, ImageProps } from 'antd';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { ReactComponent as EyeSVG } from 'assets/img/icons/eye.svg';
 import { ReactComponent as RadioSelect } from 'assets/img/icons/radio-select.svg';
 import { ReactComponent as Radio } from 'assets/img/icons/radio.svg';
-import { useCallback, useEffect, useMemo, useState } from 'react';
 import useResponsive from 'hooks/useResponsive';
 
 export type TAIImage = ISchrodingerImages['images'][0];
@@ -20,26 +21,48 @@ interface IAIImageProps {
 
 function AIImage({ src, active, item, onSelect }: IAIImageProps) {
   const { isLG } = useResponsive();
+  const [show, setShow] = useState(false);
 
   const imageSize = useMemo(() => (isLG ? 103 : 108), [isLG]);
+  const preview = useMemo<ImageProps['preview']>(() => {
+    return isLG
+      ? {
+          visible: show,
+          maskClassName: 'rounded-lg !opacity-0',
+          onVisibleChange: () => setShow(false),
+        }
+      : {
+          maskClassName: 'rounded-lg',
+        };
+  }, [isLG, show]);
 
-  const onClick = () => {
+  const onClick = useCallback(() => {
     onSelect(item);
-  };
+  }, [item, onSelect]);
 
   return (
     <div className="relative bg-[#F5FEF7] w-[103px]] h-[103px] lg:w-[108px] lg:h-[108px] rounded-lg">
-      <div className="absolute w-[32px] h-[32px] top-[6px] right-0 cursor-pointer z-10" onClick={onClick}>
+      <div
+        className="flex justify-center items-center absolute w-[32px] h-[32px] right-0 cursor-pointer z-10"
+        onClick={onClick}>
         {active ? <RadioSelect /> : <Radio />}
       </div>
       <Image
+        id="ai-image"
         className="object-contain rounded-lg"
         src={src}
         width={imageSize}
         height={imageSize}
         alt="AI-image"
-        preview={{ maskClassName: 'rounded-lg' }}
+        preview={preview}
       />
+      {isLG && (
+        <div
+          className="absolute bottom-3 left-3 flex justify-center items-center rounded-md px-1 py-2 bg-fillMask1 w-[24px] h-[24px]"
+          onClick={() => setShow(true)}>
+          <EyeSVG />
+        </div>
+      )}
     </div>
   );
 }
