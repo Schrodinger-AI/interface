ac1523c8ea5801405538f2f932c4a01d89f67275
huge <huge@schrodingernft.ai>
2024-03-13 15:30:39 +0800

    feat: message add safe top

--- src/components/SafeArea/index.tsx
+++ src/components/SafeArea/index.tsx
@@ -1,8 +1,16 @@
+import { message } from 'antd';
 import useSafeAreaHeight from 'hooks/useSafeAreaHeight';
-import React from 'react';
+import { useEffect } from 'react';
 
 export default function SafeArea({ children }: { children: React.ReactNode }) {
   const { topSafeHeight } = useSafeAreaHeight();
+
+  useEffect(() => {
+    message.config({
+      top: 8 + Number(topSafeHeight || 0),
+    });
+  }, [topSafeHeight]);
+
   return (
     <div className="h-full" style={{ paddingTop: topSafeHeight }}>
       {children}
