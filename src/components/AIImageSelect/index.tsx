21f992ece48f00591629ed52be4c45acec14bc30
Desmondwi <desmond@schrodingernft.ai>
2024-03-16 18:13:45 +0800

    feat: 🎸 add image reload

--- src/components/AIImageSelect/index.tsx
+++ src/components/AIImageSelect/index.tsx
@@ -1,13 +1,13 @@
 import { Image, ImageProps } from 'antd';
+import { Button } from 'aelf-design';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { ReactComponent as EyeSVG } from 'assets/img/icons/eye.svg';
 import { ReactComponent as RadioSelect } from 'assets/img/icons/radio-select.svg';
 import DefaultCatImg from 'assets/img/icons/defaultCat.png';
 import useResponsive from 'hooks/useResponsive';
 import clsx from 'clsx';
-
 import styles from './style.module.css';
-
+import uriToHttp from 'utils/format';
 interface IAIImageProps {
   src: string;
   active: boolean;
@@ -19,7 +19,11 @@ function AIImage({ src, active, index, onSelect }: IAIImageProps) {
   const { isLG } = useResponsive();
   const [show, setShow] = useState(false);
   const [error, setError] = useState(false);
-  const [random, setRandom] = useState<number>(0);
+  const [srcIndex, setSrcIndex] = useState<number>(0);
+  const [imgUrl, setImageUrl] = useState<string>('');
+
+  const srcList = useMemo(() => uriToHttp(src).concat(DefaultCatImg.src), [src]);
+  useEffect(() => setImageUrl(srcList[srcIndex]), [srcIndex, srcList]);
 
   const preview = useMemo<ImageProps['preview']>(() => {
     return isLG
@@ -40,14 +44,34 @@ function AIImage({ src, active, index, onSelect }: IAIImageProps) {
 
   const onError = useCallback(() => {
     console.log('image-load-error');
-    setError(true);
-    setShow(false);
-  }, [setError, setShow]);
+    const _index = srcIndex + 1;
+    const defaultImgIndex = srcList.length - 1;
+
+    if (_index >= defaultImgIndex) {
+      setError(true);
+      setShow(false);
+      setSrcIndex(defaultImgIndex);
+      setImageUrl(srcList[defaultImgIndex]);
+      return;
+    }
+    setSrcIndex(_index);
+  }, [srcIndex, srcList]);
 
   const onReload = useCallback(() => {
-    setError(false);
-    setRandom(Date.now());
-  }, []);
+    console.log('onReload');
+    const urlFirst = srcList[0];
+
+    if (!urlFirst.startsWith('http')) return;
+
+    const _imgUrl = new URL(urlFirst);
+    _imgUrl.searchParams.append('stamp', Date.now().toString());
+    setImageUrl(_imgUrl.toString());
+  }, [srcList]);
+
+  const onLoad = useCallback(() => {
+    console.log('onLoad');
+    setError(srcIndex === srcList.length - 1);
+  }, [srcIndex, srcList.length]);
 
   const Radio = useMemo(() => {
     return (
@@ -69,20 +93,21 @@ function AIImage({ src, active, index, onSelect }: IAIImageProps) {
         'relative border-solid bg-[#F5FEF7] flex-1 aspect-square lg:flex-none lg:w-[120px] lg:h-[120px] rounded-lg overflow-hidden',
         active ? 'border-[2px] border-brandDefault' : 'border border-neutralBorder',
       )}>
-      <div
-        className="flex justify-center items-center absolute w-[32px] h-[32px] right-[8px] top-[8px] cursor-pointer z-10"
-        onClick={onClick}>
-        {Radio}
-      </div>
+      {!error && (
+        <div
+          className="flex justify-center items-center absolute w-[32px] h-[32px] right-[8px] top-[8px] cursor-pointer z-10"
+          onClick={onClick}>
+          {Radio}
+        </div>
+      )}
       <Image
         id="ai-image"
         className={clsx('w-full h-full')}
-        // src={`${src}?${random}`}
-        src={src}
+        src={imgUrl}
         alt="AI-image"
         preview={preview}
         onError={onError}
-        fallback={DefaultCatImg.src}
+        onLoad={onLoad}
       />
       {isLG && !error && (
         <div
@@ -91,13 +116,13 @@ function AIImage({ src, active, index, onSelect }: IAIImageProps) {
           <EyeSVG />
         </div>
       )}
-      {/* {error && (
-        <div className="absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center bg-fillMask1 rounded-lg">
+      {error && (
+        <div className="absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center bg-fillMask1 rounded-lg lg:opacity-0 hover:lg:opacity-100 cursor-pointer">
           <Button type="primary" size="small" onClick={onReload}>
             Reload
           </Button>
         </div>
-      )} */}
+      )}
     </div>
   );
 }
