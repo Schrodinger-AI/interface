f30703b54380fb7ec326f78268168714273878f1
patrick-schrodinger <patrick.star@schrodingernft.ai>
2024-03-16 18:49:56 +0800

    feat: 🎸 add DynamicLoading

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,4 +1,8 @@
 'use client';
-
+import { DynamicLoading } from 'components/DynamicLoading';
 import dynamic from 'next/dynamic';
-export default dynamic(() => import('pageComponents/detail'), { ssr: false });
+
+export default dynamic(() => import('pageComponents/detail'), {
+  ssr: false,
+  loading: () => <DynamicLoading />,
+});
