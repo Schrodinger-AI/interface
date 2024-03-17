85a91edd5d4cb0f87228ee594dad01bfef1cb4f8
leosley-m <leosley@schrodingernft.ai>
2024-03-17 21:21:15 +0800

    feat: adopt2 modal optimization

--- src/components/SGRTokenInfo/index.tsx
+++ src/components/SGRTokenInfo/index.tsx
@@ -1,20 +1,31 @@
+import React from 'react';
+import styles from './index.module.css';
 export interface ISGRTokenInfoProps {
   tokenName?: string;
   symbol?: string;
   amount?: string | number;
 }
 
-export default function SGRTokenInfo({ tokenName, symbol, amount }: ISGRTokenInfoProps) {
+function SGRTokenInfo({ tokenName, symbol, amount }: ISGRTokenInfoProps) {
   return (
-    <div className="text-base text-[#919191]">
-      <div className="flex justify-between">
-        <span>{symbol ?? '--'}</span>
-        <span>Owned Amount</span>
-      </div>
-      <div className="flex justify-between text-[#1A1A1A]">
-        <span className="text-lg font-semibold">{tokenName ?? '--'}</span>
-        <span className="font-medium">{amount ?? '--'}</span>
+    <div className={styles['token-info']}>
+      <div className="text-lg font-medium text-neutralTitle">Info</div>
+      <div className="mt-[16px]">
+        <div className={styles.item}>
+          <span className={styles.title}>Name</span>
+          <span className={styles.value}>{tokenName ?? '--'}</span>
+        </div>
+        <div className={styles.item}>
+          <span className={styles.title}>Symbol</span>
+          <span className={styles.value}>{symbol ?? '--'}</span>
+        </div>
+        <div className={styles.item}>
+          <span className={styles.title}>Amount Owned</span>
+          <span className={styles.value}>{amount ?? '--'}</span>
+        </div>
       </div>
     </div>
   );
 }
+
+export default React.memo(SGRTokenInfo);
