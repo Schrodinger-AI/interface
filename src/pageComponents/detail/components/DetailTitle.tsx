18d12d10a0b60480889d161e7c1beb6554665904
leosley-m <leosley@schrodingernft.ai>
2024-03-18 17:54:46 +0800

    feat: detail page adaptation

--- src/pageComponents/detail/components/DetailTitle.tsx
+++ src/pageComponents/detail/components/DetailTitle.tsx
@@ -3,6 +3,7 @@ import { TSGRToken } from 'types/tokens';
 import { divDecimals } from 'utils/calculate';
 import styles from './styles.module.css';
 import clsx from 'clsx';
+import TextEllipsis from 'components/TextEllipsis';
 
 export default function DetailTitle({ detail }: { detail: TSGRToken }) {
   const amountStr = useMemo(
@@ -11,18 +12,18 @@ export default function DetailTitle({ detail }: { detail: TSGRToken }) {
   );
 
   return (
-    <div className="w-full mr-0 lg:mr-[12px] h-[58px] lg:h-[68px] flex justify-between lg:justify-start">
+    <div className="w-full lg:w-[450px] mr-0 lg:mr-[40px] flex justify-between lg:justify-start">
       <div className={styles.card}>
         <div className={styles.title}>Name</div>
-        <div className={styles.value}>{detail.tokenName}</div>
+        <TextEllipsis value={detail.tokenName} className={styles.value} />
       </div>
-      <div className={clsx(styles.card, 'ml-[16px] lg:ml-[68px]')}>
+      <div className={clsx(styles.card, 'ml-[16px]')}>
         <div className={styles.title}>Symbol</div>
-        <div className={styles.value}>{detail.symbol}</div>
+        <TextEllipsis value={detail.symbol} className={styles.value} />
       </div>
-      <div className={clsx(styles.card, 'ml-[16px] lg:ml-[68px]')}>
-        <div className={clsx(styles.title, 'min-w-[102px]')}>Amount Owned</div>
-        <div className={clsx(styles.value, 'text-right lg:text-left')}>{amountStr}</div>
+      <div className={clsx(styles.card, 'ml-[16px]')}>
+        <div className={clsx(styles.title, 'min-w-[102px] whitespace-nowrap')}>Amount Owned</div>
+        <div className={clsx(styles.value, 'text-right')}>{amountStr}</div>
       </div>
     </div>
   );
