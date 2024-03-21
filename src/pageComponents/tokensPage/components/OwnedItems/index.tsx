4cd8b03ba4acecbd8f9736257935ee448bc62484
Desmondwi <desmond@schrodingernft.ai>
2024-03-21 20:04:11 +0800

    refactor: 💡 list add common ScrollContent

--- src/pageComponents/tokensPage/components/OwnedItems/index.tsx
+++ src/pageComponents/tokensPage/components/OwnedItems/index.tsx
@@ -20,7 +20,7 @@ import { ISubTraitFilterInstance } from 'components/SubTraitFilter';
 import FilterTags from '../FilterTags';
 import { CollapseForPC, CollapseForPhone } from '../FilterContainer';
 import FilterMenuEmpty from '../FilterMenuEmpty';
-import ScrollContent from '../ScrollContent';
+// import ScrollContent from '../ScrollContent';
 import { divDecimals, getPageNumber } from 'utils/calculate';
 import { useDebounceFn } from 'ahooks';
 import useResponsive from 'hooks/useResponsive';
@@ -31,6 +31,11 @@ import { store } from 'redux/store';
 import { TGetSchrodingerListParams, useGetSchrodingerList, useGetTraits } from 'graphqlServer';
 import { ZERO } from 'constants/misc';
 import { TSGRItem } from 'types/tokens';
+import ScrollContent from 'components/ScrollContent';
+import { CardType } from 'components/ItemCard';
+import useColumns from 'hooks/useColumns';
+import { EmptyList } from 'components/EmptyList';
+import { useRouter } from 'next/navigation';
 
 export default function OwnedItems() {
   const { wallet } = useWalletService();
@@ -53,6 +58,9 @@ export default function OwnedItems() {
   const [moreLoading, setMoreLoading] = useState<boolean>(false);
   const { showLoading, closeLoading, visible: isLoading } = useLoading();
   const pageSize = 32;
+  const gutter = useMemo(() => (isLG ? 12 : 20), [isLG]);
+  const column = useColumns(collapsed);
+  const router = useRouter();
   const walletAddress = useMemo(() => wallet.address, [wallet.address]);
   const siderWidth = useMemo(() => {
     if (is2XL) {
@@ -94,12 +102,15 @@ export default function OwnedItems() {
       if (!params.chainId || !params.address) {
         return;
       }
-      if (loadMore) {
-        setMoreLoading(true);
-      } else {
-        isLoadMore.current = false;
-        showLoading();
-      }
+      // if (loadMore) {
+      //   setMoreLoading(true);
+      // } else {
+      //   isLoadMore.current = false;
+      //   showLoading();
+      // }
+      showLoading();
+      // if (!loadMore) isLoadMore.current = false;
+
       try {
         const {
           data: { getSchrodingerList: res },
@@ -117,7 +128,8 @@ export default function OwnedItems() {
             amount: divDecimals(item.amount, item.decimals).toFixed(),
           };
         });
-        if (isLoadMore.current) {
+        // if (isLoadMore.current) {
+        if (loadMore) {
           setDataSource((preData) => [...(preData || []), ...data]);
         } else {
           setDataSource(data);
@@ -337,8 +349,8 @@ export default function OwnedItems() {
   }, [filterSelect, searchParam]);
 
   const loadMoreData = useCallback(() => {
-    if (isLoading || !hasMore || moreLoading) return;
-    isLoadMore.current = true;
+    if (isLoading || !hasMore) return;
+    // isLoadMore.current = true;
     SetCurrent(current + 1);
     fetchData({
       params: {
@@ -347,7 +359,23 @@ export default function OwnedItems() {
       },
       loadMore: true,
     });
-  }, [isLoading, hasMore, moreLoading, current, fetchData, requestParams]);
+  }, [isLoading, hasMore, current, fetchData, requestParams]);
+
+  const emptyText = useMemo(() => {
+    return (
+      dataSource && (
+        <Flex className="pt-0 lg:pt-6" justify="center" align="center">
+          <EmptyList isChannelShow={!ownedTotal} defaultDescription="No inscriptions found" />
+        </Flex>
+      )
+    );
+  }, [dataSource, ownedTotal]);
+  const onPress = useCallback(
+    (item: TSGRItem) => {
+      router.push(`/detail?symbol=${item.symbol}`);
+    },
+    [router],
+  );
 
   return (
     <div>
@@ -412,7 +440,7 @@ export default function OwnedItems() {
               clearSearchChange={clearSearchChange}
             />
           </Flex>
-          <ScrollContent
+          {/* <ScrollContent
             collapsed={collapsed}
             ListProps={{
               dataSource,
@@ -422,6 +450,14 @@ export default function OwnedItems() {
               loading: moreLoading,
               loadMore: loadMoreData,
             }}
+          /> */}
+          <ScrollContent
+            type={CardType.MY}
+            grid={{ gutter, column }}
+            emptyText={emptyText}
+            onPress={onPress}
+            loadMore={loadMoreData}
+            ListProps={{ dataSource }}
           />
         </Layout>
       </Layout>
