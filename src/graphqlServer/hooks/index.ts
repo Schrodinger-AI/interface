c32902673be4de34fd5918fac531339e3e92a96b
huge <huge@schrodingernft.ai>
2024-04-17 22:08:21 +0800

    feat: fix some problems

--- src/graphqlServer/hooks/index.ts
+++ src/graphqlServer/hooks/index.ts
@@ -35,15 +35,14 @@ export const useGetSchrodingerDetail = () => {
   );
 };
 
-export const useGetTraits = (type: ListTypeEnum) => {
+export const useGetTraits = () => {
   const client = useGraphQLClient();
-  const requestApi = useMemo(() => {
-    return type === ListTypeEnum.My ? getTraits : getAllTraits;
-  }, [type]);
-  return useCallback(
-    (params: TGraphQLParamsType<typeof requestApi>) => requestApi(client, params),
-    [client, requestApi],
-  );
+  return useCallback((params: TGraphQLParamsType<typeof getTraits>) => getTraits(client, params), [client]);
+};
+
+export const useGetAllTraits = () => {
+  const client = useGraphQLClient();
+  return useCallback((params: TGraphQLParamsType<typeof getAllTraits>) => getAllTraits(client, params), [client]);
 };
 
 export const useGetSubTraits = (type: ListTypeEnum) => {
