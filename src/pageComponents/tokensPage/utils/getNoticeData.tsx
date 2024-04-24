ba566810471361cd8990c09e2efd8245bc8a88e1
leosley-m <leosley@schrodingernft.ai>
2024-04-24 19:50:24 +0800

    feat: 🎸 optimize copywriting

--- src/pageComponents/tokensPage/utils/getNoticeData.tsx
+++ src/pageComponents/tokensPage/utils/getNoticeData.tsx
@@ -1,5 +1,8 @@
 import { IScrollAlertItem } from 'components/ScrollAlert';
 import { TCustomizationItemType, TGlobalConfigType } from 'redux/types/reducerTypes';
+import { OmittedType, addPrefixSuffix, getOmittedStr } from 'utils/addressFormatting';
+import { divDecimals } from 'utils/calculate';
+import { formatTokenPrice } from 'utils/format';
 import { openExternalLink } from 'utils/openlink';
 
 const onJump = (symbol: string, cmsInfo?: TCustomizationItemType & TGlobalConfigType) => {
@@ -8,48 +11,57 @@ const onJump = (symbol: string, cmsInfo?: TCustomizationItemType & TGlobalConfig
   openExternalLink(`${forestUrl}/detail/buy/${cmsInfo?.curChain}-${symbol}/${cmsInfo?.curChain}`, '_blank');
 };
 
+const mockData = [
+  {
+    symbol: 'SGR-7649',
+    amount: 105309200,
+    decimals: 8,
+    dealTime: 1711957656365,
+    price: 2200000000,
+    fromAddress: 'rA2p1UHcjk5V9A4kfGWhpK3sjXywYEyfcb5aYjpHmYUFENHXF',
+  },
+  {
+    symbol: 'SGR-7650',
+    amount: 105309200,
+    decimals: 8,
+    dealTime: 1711957656365,
+    price: 23400000000,
+    fromAddress: 'rA2p1UHcjk5V9A4kfGWhpK3sjXywYEyfcb5aYjpHmYUFENHXF',
+  },
+  {
+    symbol: 'SGR-7651',
+    amount: 105309200,
+    decimals: 8,
+    dealTime: 1711957656365,
+    price: 63400000000,
+    fromAddress: 'rA2p1UHcjk5V9A4kfGWhpK3sjXywYEyfcb5aYjpHmYUFENHXF',
+  },
+  {
+    symbol: 'SGR-7652',
+    amount: 105309200,
+    decimals: 8,
+    dealTime: 1711957656365,
+    price: 600000000,
+    fromAddress: 'rA2p1UHcjk5V9A4kfGWhpK3sjXywYEyfcb5aYjpHmYUFENHXF',
+  },
+];
+
 export const getNoticeData: (cmsInfo?: TCustomizationItemType & TGlobalConfigType) => IScrollAlertItem[] = (
   cmsInfo?: TCustomizationItemType & TGlobalConfigType,
 ) => {
   try {
-    return [
-      {
+    return mockData.map((item) => {
+      return {
         text: (
           <span>
-            ELF_rA2p...NHXF_tDVW <span className="text-warning600">100</span> SGRTEST-8425GEN9 with{' '}
-            <span className="text-warning600">300ELF</span>
+            {item.symbol} purchased for{' '}
+            <span className="text-warning600">{formatTokenPrice(divDecimals(item.price, item.decimals))} ELF</span> by{' '}
+            {getOmittedStr(addPrefixSuffix(item.fromAddress), OmittedType.ADDRESS)}
           </span>
         ),
-        handle: () => onJump('SGRTEST-7536', cmsInfo),
-      },
-      {
-        text: (
-          <span>
-            ELF_rA2p...NHXF_tDVW <span className="text-warning600">200</span> SGRTEST-8425GEN9 with{' '}
-            <span className="text-warning600">300ELF</span>
-          </span>
-        ),
-        handle: () => onJump('SGRTEST-7536', cmsInfo),
-      },
-      {
-        text: (
-          <span>
-            ELF_rA2p...NHXF_tDVW <span className="text-warning600">300</span> SGRTEST-8425GEN9 with{' '}
-            <span className="text-warning600">300ELF</span>
-          </span>
-        ),
-        handle: () => onJump('SGRTEST-7536', cmsInfo),
-      },
-      {
-        text: (
-          <span>
-            ELF_rA2p...NHXF_tDVW <span className="text-warning600">400</span> SGRTEST-8425GEN9 with{' '}
-            <span className="text-warning600">300ELF</span>
-          </span>
-        ),
-        handle: () => onJump('SGRTEST-7536', cmsInfo),
-      },
-    ];
+        handle: () => onJump(item.symbol, cmsInfo),
+      };
+    });
   } catch (error) {
     return [];
   }
