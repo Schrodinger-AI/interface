a84a30d680b1868055dcfee3e561e490bdc274db
leosley-m <leosley@schrodingernft.ai>
2024-04-24 20:06:25 +0800

    feat: 🎸 hide notice

--- src/pageComponents/tokensPage/index.tsx
+++ src/pageComponents/tokensPage/index.tsx
@@ -2,25 +2,12 @@
 import TokensInfo from 'components/TokensInfo';
 import OwnedItems from 'components/OwnedItems';
 import { ListTypeEnum } from 'types';
-import { useEffect, useState } from 'react';
-import { useCmsInfo } from 'redux/hooks';
-import { IScrollAlertItem } from 'components/ScrollAlert';
-import { getNoticeData } from './utils/getNoticeData';
 
 export default function TokensPage() {
-  const cmsInfo = useCmsInfo();
-
-  const [noticeData, setNoticeData] = useState<IScrollAlertItem[]>([]);
-
-  useEffect(() => {
-    const res = getNoticeData(cmsInfo);
-    setNoticeData(res);
-  }, [cmsInfo]);
-
   return (
     <div className="flex flex-col max-w-[2560px] w-full">
       <TokensInfo />
-      <OwnedItems noticeData={noticeData} pageState={ListTypeEnum.All} />
+      <OwnedItems pageState={ListTypeEnum.All} />
     </div>
   );
 }
