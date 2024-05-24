daffbaa2072cbf5c57c0d5a7736b95130b0676f7
leosley-m <leosley@schrodingernft.ai>
2024-05-24 15:36:08 +0800

    feat: 🎸 empty data display compatible list style

--- src/components/EmptyList/components/Introduction/index.tsx
+++ src/components/EmptyList/components/Introduction/index.tsx
@@ -1,42 +1,15 @@
 import { Collapse } from 'aelf-design';
 import React, { useState } from 'react';
 import styles from './index.module.css';
-import { TEmptyChannelIntroduction, TEmptyChannelIntroductionStep } from 'types/misc';
+import { TEmptyChannelIntroduction } from 'types/misc';
 import clsx from 'clsx';
 import { ReactComponent as ArrowDownIcon } from 'assets/img/arrow.svg';
 import { Col, Row } from 'antd';
-import SkeletonImage from 'components/SkeletonImage';
 import { useResponsive } from 'hooks/useResponsive';
+import StepCard from '../StepCard';
+import IntroductionList from '../IntroductionList';
 
-function StepCard({ title, description, card }: TEmptyChannelIntroductionStep) {
-  return (
-    <div>
-      <p className="text-sm text-neutralPrimary font-medium">{title}</p>
-      {description ? (
-        <div className="h-auto lg:h-[40px]">
-          {description.map((item, index) => {
-            return (
-              <span key={index} className="text-neutralSecondary text-xs">
-                {item}
-              </span>
-            );
-          })}
-        </div>
-      ) : null}
-      {card && card.image ? (
-        <div className="mt-[8px] shadow-cardShadow p-[12px] rounded-lg">
-          <SkeletonImage
-            img={card.image}
-            className="w-full lg:max-h-[120px] !rounded-none"
-            imageClassName="!rounded-none"
-          />
-        </div>
-      ) : null}
-    </div>
-  );
-}
-
-function Introduction({ title, step }: TEmptyChannelIntroduction) {
+function Introduction({ title, step, list }: TEmptyChannelIntroduction) {
   const [rotate, setRotate] = useState<boolean>(false);
   const { isLG } = useResponsive();
 
@@ -63,18 +36,30 @@ function Introduction({ title, step }: TEmptyChannelIntroduction) {
               </span>
             ),
             children: (
-              <Row gutter={[16, 16]} className="px-[8px] pb-[8px]">
-                {step.map((info, index) => {
-                  return (
-                    <Col span={isLG ? 24 : 12} key={index}>
-                      <StepCard {...info} />
-                    </Col>
-                  );
-                })}
-              </Row>
+              <div>
+                {step && step.length ? (
+                  <Row gutter={[16, 16]} className="px-[8px] pb-[8px]">
+                    {step.map((info, index) => {
+                      return (
+                        <Col span={isLG ? 24 : 12} key={index}>
+                          <StepCard {...info} />
+                        </Col>
+                      );
+                    })}
+                  </Row>
+                ) : null}
+                {list && list.length ? (
+                  <div className={clsx(step && step.length ? 'mt-[16px]' : '')}>
+                    {list.map((info, index) => {
+                      return <IntroductionList key={index} {...info} />;
+                    })}
+                  </div>
+                ) : null}
+              </div>
             ),
           },
-        ]}></Collapse>
+        ]}
+      />
     </div>
   );
 }
