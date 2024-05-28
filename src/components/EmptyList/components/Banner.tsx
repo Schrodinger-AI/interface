804ee630199f8e33d4bd81b1a86af58496bf97ce
leosley-m <leosley@schrodingernft.ai>
2024-05-28 17:25:43 +0800

    feat: 🎸 add ga to etransfer

--- src/components/EmptyList/components/Banner.tsx
+++ src/components/EmptyList/components/Banner.tsx
@@ -1,18 +1,27 @@
 import clsx from 'clsx';
 import SkeletonImage from 'components/SkeletonImage';
+import { etransferDomain } from 'constants/url';
 import { useJumpToPage } from 'hooks/useJumpToPage';
 import { useResponsive } from 'hooks/useResponsive';
 import React from 'react';
 import { TEmptyChannelBanner } from 'types/misc';
+import { AdTracker } from 'utils/ad';
 
 function Banner({ banner }: { banner: TEmptyChannelBanner }) {
   const { isLG } = useResponsive();
   const { jumpToPage } = useJumpToPage();
 
+  const onBannerClick = () => {
+    jumpToPage({ link: banner?.link, linkType: banner?.linkType });
+    if (banner?.link?.includes(etransferDomain)) {
+      AdTracker.trackEvent('user_click_etransfer_noneSGR_test');
+    }
+  };
+
   return (
     <div
       className={clsx('flex w-full h-auto first:mt-0 mt-[8px] overflow-hidden', banner?.link ? 'cursor-pointer' : '')}
-      onClick={() => jumpToPage({ link: banner?.link, linkType: banner?.linkType })}>
+      onClick={onBannerClick}>
       <SkeletonImage
         img={isLG ? banner.imgUrl.mobile || '' : banner.imgUrl.pc || ''}
         className="w-full h-auto"
