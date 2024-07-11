23f35019e527377bc092ae2b1a19a0a84f229651
leosley-m <leosley@schrodingernft.ai>
2024-07-11 19:29:46 +0800

    feat: 🎸 optimize interface parameter transmission

--- src/pageComponents/events-detail/components/EventsTable.tsx
+++ src/pageComponents/events-detail/components/EventsTable.tsx
@@ -3,18 +3,23 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { IEventsDetailListTable } from '../types/type';
 import { getEventRankList } from 'api/request';
 import CustomTable from 'components/CustomTable';
+import { useParams } from 'next/navigation';
 
 function EventsTable({
   header: th,
   data,
   server,
   isFinal = false,
+  params = {},
 }: IEventsDetailListTable & {
   isFinal?: boolean;
 }) {
   const [header, setHeader] = useState<IEventsDetailListTable['header']>(server ? [] : th);
   const [dataSource, setDataSource] = useState<IEventsDetailListTable['data']>(server ? [] : data);
   const [loading, setLoading] = useState<boolean>(server ? true : false);
+  const { id } = useParams() as {
+    id: string;
+  };
 
   const getTableDataSource = useCallback(
     async (server: string) => {
@@ -22,6 +27,8 @@ function EventsTable({
         setLoading(true);
         const { header, data } = await getEventRankList(server, {
           isFinal,
+          activityId: id,
+          ...params,
         });
 
         setHeader(header);
