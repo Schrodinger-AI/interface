6389b32bab9c45bc47f5b7effd82f35eb31641cc
leosley-m <leosley@schrodingernft.ai>
2024-07-16 20:33:13 +0800

    feat: 🎸 add pop-up windows of TG homepage and pixel style

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,22 +1,25 @@
 import Lottie from 'lottie-react';
 import LoadingAnimation from 'assets/img/loading-animation.json';
 import LoadingAnimationBlue from 'assets/img/loading-animation-blue.json';
+import LoadingAnimationPurple from 'assets/img/loading-animation-purple.json';
 import { useMemo } from 'react';
 import React from 'react';
 
 const loadingImg = {
   white: LoadingAnimation,
   blue: LoadingAnimationBlue,
+  purple: LoadingAnimationPurple,
 };
 
 const sizeStyle = {
   default: 'w-[40px] h-[40px]',
-  small: 'w-[24px] h-[24px]',
+  middle: 'w-[24px] h-[24px]',
+  small: 'w-[18px] h-[18px]',
 };
 
 interface IProps {
-  color?: 'white' | 'blue';
-  size?: 'default' | 'small';
+  color?: 'white' | 'blue' | 'purple';
+  size?: 'default' | 'middle' | 'small';
 }
 
 function Loading({ color = 'blue', size = 'default' }: IProps) {
