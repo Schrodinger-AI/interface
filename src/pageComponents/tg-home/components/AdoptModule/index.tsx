0e7eb2a9239b80648bb2c54772dd1f12cc64faec
leosley-m <leosley@schrodingernft.ai>
2024-08-15 20:26:46 +0800

    feat: 🎸 update

--- src/pageComponents/tg-home/components/AdoptModule/index.tsx
+++ src/pageComponents/tg-home/components/AdoptModule/index.tsx
@@ -3,6 +3,7 @@ import { Flex } from 'antd';
 import Link from 'next/link';
 import { ReactComponent as QuestionSVG } from 'assets/img/icons/question.svg';
 import clsx from 'clsx';
+import { ReactComponent as CatSVG } from 'assets/img/telegram/cat.svg';
 
 export default function AdoptModule({ onAdopt }: { onAdopt: () => void }) {
   return (
@@ -10,6 +11,8 @@ export default function AdoptModule({ onAdopt }: { onAdopt: () => void }) {
       className="p-4 border-[2px] border-dashed border-pixelsPrimaryTextPurple bg-pixelsModalBg tg-card-shadow z-10 relative"
       vertical
       gap={16}>
+      <CatSVG className="w-[70px] h-[60px] absolute left-[34px] -top-9" />
+
       <div className="w-full flex justify-between items-center">
         <span className={clsx('dark-title text-2xl font-semibold')}>Adopt a cat</span>
         <Link href="/telegram/rules">
@@ -33,6 +36,21 @@ export default function AdoptModule({ onAdopt }: { onAdopt: () => void }) {
           );
         })}
       </div>
+      {/* <img
+        src={require('assets/img/telegram/ball-1.png').default.src}
+        alt=""
+        className="w-[72px] h-[96px] absolute -left-4 -bottom-[99px] z-0"
+      />
+      <img
+        src={require('assets/img/telegram/ball-2.png').default.src}
+        alt=""
+        className="w-[62.2px] h-16 absolute  -right-4 -bottom-[37px] z-0"
+      />
+      <img
+        src={require('assets/img/telegram/cloud.png').default.src}
+        alt=""
+        className="w-[217.6px] h-10 absolute -right-4 -top-[37px]"
+      /> */}
     </Flex>
   );
 }
