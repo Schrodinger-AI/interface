59a59415fbf5878bb6eda0e4f1a979fb6d8f7833
leosley-m <leosley@schrodingernft.ai>
2024-09-25 11:31:01 +0800

    feat: 🎸 update schrodingerGQL url

--- build.config/rewrites/test.js
+++ build.config/rewrites/test.js
@@ -5,7 +5,7 @@ module.exports = [
     source: '/schrodingerGQL/:path*',
     destination:
       // 'https://test-indexer.schrodingerai.com/SchrodingerIndexer_DApp/SchrodingerIndexerPluginSchema/graphql/:path*',
-      'https://app-testnet.aefinder.io/schrodinger/0424337298924ea5b017063de749e395/graphql/:path*',
+      'https://app-testnet.aefinder.io/schrodinger/e3a8b6cdf945455183d3c0cd024f9618/graphql/:path*',
   },
   {
     source: '/forestGQL/:path*',
