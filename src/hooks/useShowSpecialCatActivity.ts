f6ee3bc8020b41546b8fc0fb9ccf731088e5222c
leosley-m <leosley@schrodingernft.ai>
2024-09-30 10:05:04 +0800

    feat: 🎸 update activity modal

--- src/hooks/useShowSpecialCatActivity.ts
+++ src/hooks/useShowSpecialCatActivity.ts
@@ -5,10 +5,12 @@ import SpecialCatActivityTipsModal from 'components/SpecialCatActivityTipsModal'
 import { sleep } from '@portkey/utils';
 import BigNumber from 'bignumber.js';
 import useTelegram from './useTelegram';
+import { useWalletService } from './useWallet';
 
 export const useShowSpecialCatActivity = () => {
   const cmsInfo = useCmsInfo();
   const { isInTG } = useTelegram();
+  const { wallet } = useWalletService();
   const specialCatActivityTipsModal = useModal(SpecialCatActivityTipsModal);
 
   const toShow = useCallback(
@@ -29,21 +31,47 @@ export const useShowSpecialCatActivity = () => {
     return false;
   }, [cmsInfo?.specialCatActivity]);
 
+  const addressHash = useMemo(() => {
+    return Buffer.from(wallet.address).toString('hex');
+  }, [wallet.address]);
+
+  const saveLoggedInAccounts = useCallback(
+    (loggedInAccounts: string[]) => {
+      const accounts = [...loggedInAccounts, addressHash];
+      localStorage.setItem('specialCatActivityHash', JSON.stringify(accounts));
+    },
+    [addressHash],
+  );
+
   const showSpecialCatActivity = useCallback(async () => {
     const showSpecialCatActivityStatus = localStorage.getItem('showSpecialCatActivityStatus');
+
+    let loggedInAccounts = [];
+    try {
+      loggedInAccounts = JSON.parse(localStorage.getItem('specialCatActivityHash') || '');
+    } catch (error) {
+      /* empty */
+    }
+
     const activityStartTime = String(cmsInfo?.specialCatActivity?.time?.[0]);
 
     if (isEnded) return;
 
     if (activityStartTime && !showSpecialCatActivityStatus) {
+      saveLoggedInAccounts(loggedInAccounts);
       toShow(activityStartTime);
       return;
     }
-    if (activityStartTime && showSpecialCatActivityStatus && activityStartTime !== showSpecialCatActivityStatus) {
+    if (activityStartTime && showSpecialCatActivityStatus) {
+      const addressHasShow = loggedInAccounts.includes(addressHash);
+      if (addressHasShow && activityStartTime === showSpecialCatActivityStatus) {
+        return;
+      }
+      if (!addressHasShow) saveLoggedInAccounts(loggedInAccounts);
       toShow(activityStartTime);
       return;
     }
-  }, [cmsInfo?.specialCatActivity, isEnded, toShow]);
+  }, [addressHash, cmsInfo?.specialCatActivity?.time, isEnded, saveLoggedInAccounts, toShow]);
 
   return { showSpecialCatActivity };
 };
