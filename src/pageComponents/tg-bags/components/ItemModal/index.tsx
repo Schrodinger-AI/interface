d74418ed6e0c695906305d8aef58fbd2d6df9d36
Leon Zhang <liam@schrodingernft.ai>
2024-10-25 15:17:46 +0800

    feat: handle adopt cat request

---
+++ src/pageComponents/tg-bags/components/ItemModal/index.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable @next/next/no-img-element */
+import NiceModal, { useModal } from '@ebay/nice-modal-react';
+import TGButton from 'components/TGButton';
+import TgModal from 'components/TgModal';
+import { Flex } from 'antd';
+import { ReactComponent as AdoptSVG } from 'assets/img/telegram/spin/Adopt.svg';
+import { ReactComponent as VoucherSVG } from 'assets/img/telegram/spin/Voucher.svg';
+import { SpinRewardType } from 'types/misc';
+import { throttle } from 'lodash-es';
+import useAdoptWithVoucher from 'hooks/useAdoptWithVoucher';
+import TGAdoptLoading from 'components/TGAdoptLoading';
+
+function ItemModal({ quantity }: { quantity: number; rewardType?: SpinRewardType }) {
+  const modal = useModal();
+  const { adoptWithVoucher } = useAdoptWithVoucher();
+  const tgAdoptLoading = useModal(TGAdoptLoading);
+
+  const handleAdopt = throttle(
+    async () => {
+      try {
+        tgAdoptLoading.show();
+        const data = await adoptWithVoucher({ tick: 'SGR' });
+        tgAdoptLoading.hide();
+        console.log(data);
+      } catch (error) {
+        /* empty */
+      }
+    },
+    700,
+    { trailing: false },
+  );
+
+  return (
+    <TgModal
+      title="Notice"
+      open={modal.visible}
+      hideHeader={false}
+      afterClose={modal.remove}
+      onOk={() => modal.hide()}
+      onCancel={() => modal.hide()}>
+      <div className="p-[8px]">
+        <Flex align="center" gap={24} vertical>
+          <img
+            src={require('assets/img/telegram/spin/ticket.png').default.src}
+            alt=""
+            className="w-[140px] h-[140px] rounded-[8px] z-10"
+          />
+          <Flex gap={4} align="center" vertical>
+            <VoucherSVG />
+            <p className="text-center text-white text-[14px] font-semibold">Quantity: {quantity}</p>
+          </Flex>
+          <p className="text-center leading-[20px] text-white text-[12px] font-medium">
+            Use your voucher to adopt a GEN9 cat for free! You get to keep Rare GEN9 cats, but be aware that Common GEN9
+            cats will run off and disappear.
+          </p>
+          <p className="text-center leading-[20px] text-white text-[12px] font-medium">Good luck!</p>
+        </Flex>
+        <TGButton type="success" className="w-full mt-[24px]" onClick={handleAdopt}>
+          <AdoptSVG />
+        </TGButton>
+      </div>
+    </TgModal>
+  );
+}
+
+export default NiceModal.create(ItemModal);
---
+++ src/pageComponents/tg-bags/components/ItemsModule/index.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable @next/next/no-img-element */
+import { Flex, List } from 'antd';
+import SkeletonImage from 'components/SkeletonImage';
+import { useModal } from '@ebay/nice-modal-react';
+import ItemModal from '../ItemModal';
+
+type Props = {
+  data: [{ src: string; amount: number }];
+};
+
+function ItemsModule({ data }: Props) {
+  const noticeModal = useModal(ItemModal);
+
+  return (
+    <List
+      grid={{
+        gutter: 15,
+        xs: 2,
+        sm: 2,
+        md: 2,
+      }}
+      dataSource={data || []}
+      renderItem={(item) => (
+        <List.Item>
+          <Flex
+            className="rounded-[8px] overflow-hidden cursor-pointer"
+            onClick={() => item.amount > 0 && noticeModal.show({ quantity: 1 })}
+            vertical>
+            <SkeletonImage
+              img={item.src}
+              imageSizeType="contain"
+              className="!rounded-none"
+              imageClassName="!rounded-none"
+            />
+            <Flex
+              align="center"
+              justify="space-between"
+              className="p-[12px] bg-pixelsDashPurple text-white shadow-tickShadow">
+              <span className="dark-btn-font text-[14px] leading-[22px] font-black">S-CAT Voucher</span>
+              <span className="font-black text-[12px] leading-[20px] text-pixelsWhiteBg">x{item.amount}</span>
+            </Flex>
+          </Flex>
+        </List.Item>
+      )}
+    />
+  );
+}
+
+export default ItemsModule;
