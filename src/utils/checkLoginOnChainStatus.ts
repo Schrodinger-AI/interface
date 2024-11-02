a931b2f7b90177e999766290c1fd34e8a89534b1
leosley-m <leosley@schrodingernft.ai>
2024-11-02 18:16:11 +0800

    feat: 🎸 update checkLoginOnChainStatus

--- src/utils/checkLoginOnChainStatus.ts
+++ src/utils/checkLoginOnChainStatus.ts
@@ -1,7 +1,7 @@
 import { LoginStatusEnum } from '@aelf-web-login/wallet-adapter-base';
 import { did } from '@portkey/did-ui-react';
 
-export const checkLoginOnChainStatus = async () => {
+export const checkLoginOnChainStatus = () => {
   console.log('=====checkLoginOnChainStatus', did.didWallet.isLoginStatus);
   const loginOnChainStatus = did.didWallet.isLoginStatus;
   if (loginOnChainStatus !== LoginStatusEnum.SUCCESS) {
