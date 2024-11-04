45ee720ddec5d0aa2ded1a6c36304b2cfbee69fb
Leon Zhang <liam@schrodingernft.ai>
2024-11-04 16:02:23 +0800

    feat: update withdraw styles

--- src/components/Header/components/WithdrawItem.tsx
+++ src/components/Header/components/WithdrawItem.tsx
@@ -1,12 +1,12 @@
 import styles from '../style.module.css';
-import { ReactComponent as WalletSVG } from 'assets/img/wallet.svg';
+import { ReactComponent as WithdrawSVG } from 'assets/img/withdraw.svg';
 import { SELL_ELF_URL } from 'constants/router';
 import React from 'react';
 
 function WithdrawItem({ checkAndRedirect }: { checkAndRedirect: (path: string) => void }) {
   return (
     <div className={styles.menuItem} onClick={() => checkAndRedirect(SELL_ELF_URL)}>
-      <WalletSVG />
+      <WithdrawSVG />
       <span>Withdraw</span>
     </div>
   );
