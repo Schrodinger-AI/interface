a0b09c3b165c048a4a6b5cb0cd470886043ea2cc
leosley-m <leosley@schrodingernft.ai>
2024-11-25 14:56:13 +0800

    feat: 🎸 add jest

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 const nextModeBabelPlugin = require('next-babel-conditional-ssg-ssr');
 
-const presets = ['next/babel'];
+const presets = ['next/babel', ['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'];
 const plugins = process.env.SKIP_SSR
   ? [nextModeBabelPlugin('ssg'), '@babel/plugin-transform-private-methods']
   : ['@babel/plugin-transform-private-methods']; // or ssg, pull from `process.env.BUILD_MODE`?
